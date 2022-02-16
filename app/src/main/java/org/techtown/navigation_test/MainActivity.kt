c664595299ce0683388d8bef3f383a508339f661
Haeng <onse3840@gmail.com>
2022-02-17 00:58:57 +0900

    jetpack_navigation_Test

--- app/src/main/java/org/techtown/navigation_test/MainActivity.kt
+++ app/src/main/java/org/techtown/navigation_test/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
 
+//test
 class MainActivity : AppCompatActivity() {
 
     lateinit var navController: NavController
