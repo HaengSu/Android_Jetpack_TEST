a71c08198979102294f806975ea8c79d99f6212c
Haeng <onse3840@gmail.com>
2022-02-17 14:31:14 +0900

    jetpack_navigation_Test(2/17)

--- app/src/main/java/org/techtown/navigation_test/MainActivity.kt
+++ app/src/main/java/org/techtown/navigation_test/MainActivity.kt
@@ -6,17 +6,15 @@ import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
 
-//test
 class MainActivity : AppCompatActivity() {
 
-    lateinit var navController: NavController
+//    lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
-
+//        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+//        val navController = navHostFragment.navController
     }
 }
--- app/src/main/java/org/techtown/navigation_test/fragment/FragmentLogin.kt
+++ app/src/main/java/org/techtown/navigation_test/fragment/FragmentLogin.kt
@@ -1,6 +1,83 @@
 package org.techtown.navigation_test.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import androidx.viewbinding.ViewBinding
+import org.techtown.navigation_test.R
+import org.techtown.navigation_test.databinding.FragmentLoginBinding
 
-class FragmentLogin : Fragment()  {
+class FragmentLogin : Fragment(), View.OnClickListener {
+
+    lateinit var navController: NavController
+
+    var mBinding: FragmentLoginBinding? = null
+    private val binding get() = mBinding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        mBinding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = Navigation.findNavController(view)
+
+//        binding.tvKakao.setOnClickListener(this)
+//        binding.tvGoogle.setOnClickListener(this)
+        binding.tvGuest.setOnClickListener(this)
+
+
+    }
+
+    override fun onClick(v : View?) {
+        when(v?.id) {
+            R.id.tv_guest -> {
+                navController.navigate(R.id.action_fragment_kakao_to_fragment_main)
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        mBinding = null
+        super.onDestroy()
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
